ddd540f570bf8154adbf86cc1274735b2dcb6375
Ionut <ghermanionut96@gmail.com>
2026-04-27 17:48:41 +0300

    Initial commit
    Signed-off-by: Ionut <ghermanionut96@gmail.com>

---
+++ config.mk
@@ -0,0 +1,12 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/JamesDSP
+
+PRODUCT_PACKAGES += \
+    JamesDSP \
+    libjamesdspaidl
+
+PRODUCT_COPY_FILES += \
+	vendor/JamesDSP/proprietary/vendor/etc/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml
+
+# Sepolicy
+BOARD_SEPOLICY_DIRS += vendor/JamesDSP/sepolicy
